9ec224958928f6a98743a5bd54ebe6915a997ee4
Thomas Allen <thomas-allen_student2021@wilder.school>
2025-05-06 09:12:10 +0200

    [SETUP-ORM] setup basic user entity with TypeORM and config use in user module

---
+++ src/users/entities/user.entity.ts
@@ -0,0 +1,19 @@
+// export class User {
+//   id: number;
+//   name: string;
+//   email: string;
+// }
+
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ length: 100 })
+  name: string;
+
+  @Column({ unique: true, length: 100 })
+  email: string;
+}
