286b518e6fa804760c9a2494c600ce3c67dd132b
Thomas Allen <thomasedwardallen@gmail.com>
2025-05-09 13:54:04 +0200

    [SEEDING] update User seeder to not truncate tables and to use track to only run once

--- src/db/seeders/user.seeder.ts
+++ src/db/seeders/user.seeder.ts
@@ -4,13 +4,14 @@ import { DataSource } from 'typeorm';
 import { Seeder, SeederFactoryManager } from 'typeorm-extension';
 
 export default class UserSeeder implements Seeder {
+  // Track ensure that the seed is only run once
+  track = true;
+
   async run(
     dataSource: DataSource,
     factoryManager: SeederFactoryManager,
   ): Promise<any> {
     console.info('Seed starting...');
-
-    await this.truncateTables(dataSource);
 
     const userFactory = factoryManager.get(User);
     const profileFactory = factoryManager.get(UserProfile);
@@ -27,16 +28,5 @@ export default class UserSeeder implements Seeder {
     }
 
     console.info('Seed completed');
-  }
-
-  private async truncateTables(dataSource: DataSource): Promise<void> {
-    await dataSource.query(`
-      TRUNCATE TABLE "user_profile" RESTART IDENTITY CASCADE
-      `);
-    await dataSource.query(`
-      TRUNCATE TABLE "user" RESTART IDENTITY CASCADE
-      `);
-
-    console.info('user_profile and user tables truncated, sequences reset');
   }
 }
