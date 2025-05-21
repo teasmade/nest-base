b9ba201e20702256860732ff0589e8378e015e06
Thomas Allen <thomasedwardallen@gmail.com>
2025-05-21 16:09:38 +0200

    [OASIS-accounts] udpate oasis accounts service to gather all data needed for partners list dashboard

--- src/oasis/oasis-accounts/oasis-accounts.service.ts
+++ src/oasis/oasis-accounts/oasis-accounts.service.ts
@@ -1,52 +1,53 @@
 import { Injectable } from '@nestjs/common';
 import { OasisHttpService } from '../oasis-auth/oasis-http.service';
-
-interface OasisAccountQueryParams {
-  $select?: string[];
-  $count?: boolean;
-  $filter?: string;
-  $orderby?: string;
-}
+import { OasisResponse } from '../oasis-auth/interfaces/oasis-response.interface';
+import { OasisAccount, OasisAccountQueryParams } from './interfaces';
 
 @Injectable()
 export class OasisAccountsService {
   constructor(private readonly oasisHttpService: OasisHttpService) {}
 
   // TODO - work out how to paginate with returned next link cookie
-  public async getOasisAccounts(pageSize?: number) {
+  public async getOasisAccounts(
+    pageSize?: number,
+  ): Promise<OasisResponse<OasisAccount>> {
     const endpoint = '/accounts';
 
-    // accountid,accountnumber,name,address1_composite,cap_siret,address1_city,cap_typedepartenairecode,cap_typedepointdegeolocalisationcode,cap_typedepartenairepointgeocode,createdon,modifiedon,cap_numtva,_cap_partenaireparentid_value
     const params: OasisAccountQueryParams = {
       $select: [
         'accountid',
         'accountnumber',
         'name',
-        'address1_composite',
-        'cap_siret',
         'address1_city',
+        'address1_stateorprovince',
+        'address1_country',
+        'address1_line1',
+        'address1_line2',
+        'address1_line3',
+        'address1_postalcode',
+        'cap_siret',
+        'cap_numtva',
         'cap_typedepartenairecode',
         'cap_typedepointdegeolocalisationcode',
         'cap_typedepartenairepointgeocode',
+        '_cap_partenaireparentid_value',
         'createdon',
         'modifiedon',
-        'cap_numtva',
-        '_cap_partenaireparentid_value',
       ],
       $count: true,
       $orderby: 'name asc',
+      $filter: 'cap_typedepartenairepointgeocode ne null',
     };
 
-    const paramsString = this.buildParams(params);
-    const response = await this.oasisHttpService.get(
+    const paramsString = this._buildParams(params);
+    const response = await this.oasisHttpService.get<OasisAccount>(
       `${endpoint}${paramsString}`,
       pageSize,
     );
     return response.data;
   }
 
-  private buildParams(params: OasisAccountQueryParams): string {
-    // ?$select=accountid,accountnumber,name,address1_composite,cap_siret,address1_city,cap_typedepartenairecode,cap_typedepointdegeolocalisationcode,cap_typedepartenairepointgeocode,createdon,modifiedon,cap_numtva,_cap_partenaireparentid_value&$count=true&$orderby=name
+  private _buildParams(params: OasisAccountQueryParams): string {
     const paramStrings: string[] = [];
 
     if (params.$select) {
