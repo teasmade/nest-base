daa9a158bb40d2acda9f4b3d2d25ccf87777385d
Thomas Allen <thomasedwardallen@gmail.com>
2025-05-26 14:27:34 +0200

    chore: add @odata.etag field to exclude value type utility, comment utility

--- src/oasis/oasis-common/utils/oasis-utility-types.ts
+++ src/oasis/oasis-common/utils/oasis-utility-types.ts
@@ -1,5 +1,13 @@
-export type NonODataFormattedValues<T> = {
-  [K in keyof T as K extends `${string}@OData.Community.Display.V1.FormattedValue`
+/***
+ * Utility type used to exclude OData values from an interface.
+ * This is used in type assertions to ensure that the interface we use
+ * to type the response from OASIS corresponds with the values we use
+ * to build the select query params.
+ */
+export type ExcludeODataValues<T> = {
+  [K in keyof T as K extends
+    | '@odata.etag'
+    | `${string}@OData.Community.Display.V1.FormattedValue`
     ? never
     : K]: T[K];
 };
