8d91dbedeebfa73908f4875ffa41482716dfae34
Thomas Allen <thomasedwardallen@gmail.com>
2025-05-27 15:45:50 +0200

    [CONTACTS] complete Oasis contact interface, complete mapping dto with return field names

--- src/contacts/dtos/oasis-contact-to-contact.dto.ts
+++ src/contacts/dtos/oasis-contact-to-contact.dto.ts
@@ -1,66 +1,89 @@
 import { Expose, Exclude } from 'class-transformer';
+import { AssertInterfaceKeysMatch } from 'src/oasis/oasis-common/utils/oasis-utility-types';
 import { OasisContact } from 'src/oasis/oasis-contacts/interfaces/oasis-contact.interface';
-
-// TODO - remove partial from partner to account dto?
 
 /**
  * DTO to map relevant Oasis Contact data structure to Orizon Contact data structure for frontend usage
  */
 export class OasisContactToContactDto implements OasisContact {
-  @Expose()
+  @Exclude()
+  '@odata.etag': string;
+
+  @Expose({ name: 'oasis_contact_id' })
   contactid: string;
 
   @Expose({ name: 'first_name' })
   firstname: string;
 
-  @Expose()
+  @Expose({ name: 'last_name' })
   lastname: string;
 
-  @Expose()
+  @Expose({ name: 'full_name' })
   fullname: string;
 
   @Expose({ name: 'email' })
   emailaddress1: string;
 
-  @Expose()
+  @Expose({ name: 'phone_number' })
   telephone1: string | null;
 
-  @Expose()
+  @Expose({ name: 'title' })
+  'cap_civilitecode@OData.Community.Display.V1.FormattedValue'?: string;
+
+  @Exclude()
   cap_civilitecode: number | null;
 
-  @Expose()
+  @Expose({ name: 'contact_type' })
+  'cap_type_contact_code@OData.Community.Display.V1.FormattedValue'?: string;
+
+  @Expose({ name: 'contact_type_code' })
   cap_type_contact_code: number | null;
 
-  @Expose()
+  @Expose({ name: 'address_line1' })
   address1_line1: string | null;
 
-  @Expose()
+  @Expose({ name: 'address_line2' })
   address1_line2: string | null;
 
-  @Expose()
+  @Expose({ name: 'address_line3' })
   address1_line3: string | null;
 
-  @Expose()
+  @Expose({ name: 'postal_code' })
   address1_postalcode: string | null;
 
-  @Expose()
+  @Expose({ name: 'city' })
   address1_city: string | null;
 
-  @Expose()
+  @Expose({ name: 'departement' })
   address1_stateorprovince: string | null;
 
-  @Expose()
+  @Expose({ name: 'country' })
   address1_country: string | null;
 
-  @Expose()
+  @Exclude()
   address1_composite: string | null;
 
-  @Expose()
+  @Expose({ name: 'referring_agency' })
+  '_cap_agence_emploi_referenteid_value@OData.Community.Display.V1.FormattedValue'?: string;
+
+  @Expose({ name: 'referring_agency_id' })
   _cap_agence_emploi_referenteid_value: string | null;
 
-  @Expose()
+  @Expose({ name: 'longitude' })
+  'address1_longitude@OData.Community.Display.V1.FormattedValue'?: string;
+
+  @Exclude()
   address1_longitude: number | null;
 
-  @Expose()
+  @Expose({ name: 'latitude' })
+  'address1_latitude@OData.Community.Display.V1.FormattedValue'?: string;
+
+  @Exclude()
   address1_latitude: number | null;
 }
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const _testImplementationKeys: AssertInterfaceKeysMatch<
+  OasisContactToContactDto,
+  OasisContact
+> = true;
--- src/oasis/oasis-contacts/interfaces/oasis-contact.interface.ts
+++ src/oasis/oasis-contacts/interfaces/oasis-contact.interface.ts
@@ -2,6 +2,7 @@ import { ExcludeODataValues } from 'src/oasis/oasis-common/utils/oasis-utility-t
 import { OasisContactSelectFields } from '../oasis-contacts.constants';
 
 export interface OasisContact {
+  '@odata.etag': string;
   contactid: string;
   firstname: string;
   lastname: string;
@@ -9,11 +10,9 @@ export interface OasisContact {
   emailaddress1: string;
   telephone1: string | null;
   cap_civilitecode: number | null;
-  'cap_civilitecode@OData.Community.Display.V1.FormattedValue'?: string | null;
+  'cap_civilitecode@OData.Community.Display.V1.FormattedValue'?: string;
   cap_type_contact_code: number | null;
-  'cap_type_contact_code@OData.Community.Display.V1.FormattedValue'?:
-    | string
-    | null;
+  'cap_type_contact_code@OData.Community.Display.V1.FormattedValue'?: string;
   address1_line1: string | null;
   address1_line2: string | null;
   address1_line3: string | null;
@@ -23,15 +22,11 @@ export interface OasisContact {
   address1_country: string | null;
   address1_composite: string | null;
   _cap_agence_emploi_referenteid_value: string | null;
-  '_cap_agence_emploi_referenteid_value@OData.Community.Display.V1.FormattedValue'?:
-    | string
-    | null;
+  '_cap_agence_emploi_referenteid_value@OData.Community.Display.V1.FormattedValue'?: string;
   address1_longitude: number | null;
-  'address1_longitude@OData.Community.Display.V1.FormattedValue'?:
-    | string
-    | null;
+  'address1_longitude@OData.Community.Display.V1.FormattedValue'?: string;
   address1_latitude: number | null;
-  'address1_latitude@OData.Community.Display.V1.FormattedValue'?: string | null;
+  'address1_latitude@OData.Community.Display.V1.FormattedValue'?: string;
 }
 
 type NonODataFormattedOasisContactKeys = keyof ExcludeODataValues<OasisContact>;
