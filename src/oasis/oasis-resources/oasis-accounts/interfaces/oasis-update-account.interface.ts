ae1bff1fd74457b140d3fadf318e55d3ce4a1ea8
Thomas Allen <thomasedwardallen@gmail.com>
2025-06-27 18:38:10 +0200

    [PARTNERS] add dto and update oasis account interface for update service points

--- src/oasis/oasis-resources/oasis-accounts/interfaces/oasis-update-account.interface.ts
+++ src/oasis/oasis-resources/oasis-accounts/interfaces/oasis-update-account.interface.ts
@@ -1,11 +1,26 @@
 export interface OasisUpdateAccountBody {
   name?: string;
+  cap_typecode?: number;
   cap_typedepartenairepointgeocode?: number;
   cap_typedepointdegeolocalisationcode?: number;
+  cap_typevehiculecode?: number;
+  'cap_Partenaireparentid@odata.bind'?: string;
   emailaddress3?: string;
   address1_line1?: string;
+  address1_line2?: string;
+  address1_line3?: string;
   address1_city?: string;
   address1_stateorprovince?: string;
   address1_postalcode?: string;
   address1_country?: string;
+  cap_siret?: string;
+  cap_numtva?: string;
+  cap_horairesdouverture?: string;
+  cap_coutmaximumvoiture?: number;
+  cap_coutmaximum2roues?: number;
+  cap_montantcautionvoiture?: number;
+  cap_montantcaution2roues?: number;
+  cap_zonedintervention?: string;
+  cap_date_debut_indisponibilite?: string;
+  cap_date_fin_indisponibilite?: string;
 }
